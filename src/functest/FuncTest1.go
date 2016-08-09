ad838d1bb3b5e1c1f9befc3806d5ae4d25e2a3cb
zhangfangqing <97105813@qq.com>
2016-08-10 00:54:34 +0800

    test

--- src/functest/FuncTest1.go
+++ src/functest/FuncTest1.go
@@ -1,14 +1,9 @@
 package main
 
-import "fmt"
-
 func add(a int , b int , c int) (resp1 int , resp2 bool) {
 	return a + b + c , true
 }
 
 func main() {
 	resp , isok :=  add(1,2,3)
-	fmt.Println(resp)
-	fmt.Println(isok)
-	fmt.Println(isok)
 }
